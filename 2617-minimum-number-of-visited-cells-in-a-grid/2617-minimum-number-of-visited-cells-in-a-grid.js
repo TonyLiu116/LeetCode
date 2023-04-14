d30752a57f6d03d35f60c4b37fcac2dd83e9196e
TonyRone <92026178+TonyRone@users.noreply.github.com>
2023-04-14 14:30:11 +0300

    Time: 7150 ms (6.45%), Space: 125.4 MB (6.45%) - LeetHub

--- 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.js
+++ 2617-minimum-number-of-visited-cells-in-a-grid/2617-minimum-number-of-visited-cells-in-a-grid.js
@@ -9,18 +9,23 @@ var minimumVisitedCells = function(grid) {
     let next = [];
     for(let i = 0 ; i < m ; i ++){
         vis.push([]);
+        next.push([]);
         for(let j = 0 ; j < n ; j ++){
             vis[i].push(0);
-            let a = i*n+j;
-            next[a] = [];
-            next[a].push(a);
-            next[a].push(a);
+            next[i].push([]);
+            next[i][j].push(i);
+            next[i][j].push(j);
         }
     }
-    function find_next(a, axis){
-        if(a >= m*n) return m*n;
-        if(next[a][axis] == a) return a;
-        return next[a][axis] = find_next(next[a][axis],axis);
+    function find_next(x,y,axis){
+        if(axis == 0){
+            if(x >= m || next[x][y][axis] == x) return x;
+            return next[x][y][axis] = find_next(next[x][y][axis],y,axis);
+        }
+        else{
+            if(y >= n || next[x][y][axis] == y) return y;
+            return next[x][y][axis] = find_next(x,next[x][y][axis],axis);
+        }
     }
     let qu = [];
     let qs = 0;
@@ -36,28 +41,21 @@ var minimumVisitedCells = function(grid) {
             return distance;
         let d = grid[x][y];
         for(let wx = x+1 ; wx <= x + d && wx < m ; wx++ ){
-            let a = wx*n+y;
-            let p = find_next(a,0);
-            if(p == m*n) break;
-            wx = Math.floor(p/n);
+            wx = find_next(wx,y,0);
             if(wx > x + d || wx >= m ) break;
             if(vis[wx][y] == 1) continue;
             vis[wx][y] = 1;
-            next[a][0] = (x+d+1)*n+y;
+            next[wx][y][0] = x+d+1;
             qu.push(wx);
             qu.push(y);
             qu.push(distance+1);
         }
         for(let wy = y+1 ; wy <= y+d && wy < n ; wy ++){
-            let a = x*n+wy;
-            let p = find_next(a,1);
-            if(p == m*n) break;
-            wy = p%n;
-            if(Math.floor(p/n) != x) break;
+            wy = find_next(x,wy,1);
             if(wy > y + d || wy >= n ) break;
             if(vis[x][wy] == 1) continue;
             vis[x][wy] = 1;
-            next[a][1] = x*n+y+d+1;
+            next[x][wy][1] = y+d+1;
             qu.push(x);
             qu.push(wy);
             qu.push(distance+1);
