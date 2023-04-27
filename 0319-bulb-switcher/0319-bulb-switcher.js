5ad6c6ad6aa26ce64745aae3564914464f67245a
TonyRone <92026178+TonyRone@users.noreply.github.com>
2023-04-27 11:11:46 +0300

    Time: 48 ms (94.83%), Space: 41.6 MB (72.41%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.js
+++ 0319-bulb-switcher/0319-bulb-switcher.js
@@ -3,26 +3,5 @@
  * @return {number}
  */
 var bulbSwitch = function(n) {
-    let check = [];
-    let S = [];
-    for(let i = 2 ; i*i <= n ; i ++){
-        if(check[i]) continue ;
-        S.push(i);
-        for(let j = i*i ; j*j <=n ; j += i)
-            check[j] = 1;
-    }
-    let count = S.length;
-    let res = 0;
-    function DFS(step, v){
-        if( step >= count || v * S[step] * S[step] > n ) return ; 
-        while(1){
-            DFS(step+1, v);
-            v = v * S[step] * S[step];
-            if(v > n) break;
-            res ++;
-        }
-    }
-    DFS(0, 1);
-    if(n > 0) res ++;
-    return res;
+   return Math.floor(Math.sqrt(n));
 };
